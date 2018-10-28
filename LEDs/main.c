f7c63a306f34792d195efb12aeca1b7fe2218f50
Eintha <eintha8@gmail.com>
2018-10-28 18:49:05 -0300

    Arreglos para cada led agregados, tambien la dimension de porta

--- LEDs/main.c
+++ LEDs/main.c
@@ -20,6 +20,12 @@ void show_port (char Port_A);//Imprime en consola el valor del puerto,bit a bit
  ******************************************************************************/
 static int input;
 /******************************************************************************/
+// Estado de los leds, 0 = apagado; 1 = encendido
+static int status [8] = {0,0,0,0,0,0,0,0};
+// Posicion inicial en x de cada led
+static int posx [8] = {29, 142, 257, 370, 481, 594, 708, 821};
+// Posicion inicial en y de cada led
+static int posy = 93;
 
 /*******************************************************************************
  * función principal, analiza el input y responde segun corresponda
@@ -32,24 +38,28 @@ int main ()
     ALLEGRO_DISPLAY *display = NULL;
     ALLEGRO_BITMAP  *image   = NULL;
 
-    if(!al_init()) {
+    if(!al_init()) 
+    {
             fprintf(stderr, "failed to initialize allegro!\n");
             return -1;
     }
 
-    if(!al_init_image_addon()) { // ADDON necesario para manejo(no olvidar el freno de mano) de imagenes 
+    if(!al_init_image_addon()) 
+    { // ADDON necesario para manejo(no olvidar el freno de mano) de imagenes 
             fprintf(stderr, "failed to initialize image addon !\n");
             return -1;
     }
 
     image = al_load_bitmap("image.jpg");
-    if(!image) {
+    if(!image) 
+    {
             fprintf(stderr, "failed to load image !\n");
             return 0;
     }
 
-    display = al_create_display(800,600);
-    if(!display) {
+    display = al_create_display(951,310);
+    if(!display) 
+    {
             al_destroy_bitmap(image);
       fprintf(stderr, "failed to create display!\n");
             return -1;
